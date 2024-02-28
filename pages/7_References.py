0aa12f9ca924b8d335391ef957dbf6f28604188c
Amr Alshatnawi <amralshatnawi@gmail.com>
2024-02-28 14:00:14 -0600

    Updated Sidebar layout

--- pages/7_References.py
+++ pages/7_References.py
@@ -15,24 +15,24 @@ st.sidebar.write("---")
 st.sidebar.markdown("""📅 March 9th, 2024""")
 st.sidebar.image("Ulogo.png")
 
-def add_side_title():
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"]::before {
-                content:"MSBI 32000 Winter 2024";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 25px;
-                position: relative;
-                top: 80px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+# def add_side_title():
+#     st.markdown(
+#         """
+#         <style>
+#             [data-testid="stSidebarNav"]::before {
+#                 content:"MSBI 32000 Winter 2024";
+#                 margin-left: 20px;
+#                 margin-top: 20px;
+#                 font-size: 25px;
+#                 position: relative;
+#                 top: 80px;
+#             }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
 
-add_side_title()
+# add_side_title()
 
 ############################# start page content #############################
 
--- 🏠Home.py
+++ 🏠Home.py
@@ -16,31 +16,31 @@ st.sidebar.markdown("""📅 March 9th, 2024""")
 
 st.sidebar.image("Ulogo.png")
 
-def add_side_title():
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"]::before {
-                content:"MSBI 32000 Winter 2024";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 25px;
-                position: relative;
-                top: 80px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+# def add_side_title():
+#     st.markdown(
+#         """
+#         <style>
+#             [data-testid="stSidebarNav"]::before {
+#                 content:"MSBI 32000 Winter 2024";
+#                 margin-left: 20px;
+#                 margin-top: 20px;
+#                 font-size: 25px;
+#                 position: relative;
+#                 top: 80px;
+#             }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
 
-add_side_title()
+# add_side_title()
 
 ############################# start page content #############################
 st.title("Exploring the Facets of COVID-19: A Multi-Question Analysis")
 st.write("✍️Authors: Amr Alshatnawi, Hailey Pangburn, Richard McMasters")
 st.write("---")
 st.header("MSBI 32000 Winter 2024 Semester - The University of Chicago")
-st.write("""Welcome to our interactive COVID-19 data web application, where we delve into the intricacies of the COVID-19 pandemic through data.
+st.write("""Welcome to our  COVID-19 data analysis web application, where we delve into the intricacies of the COVID-19 pandemic through data.
           Our mission is to uncover the hidden patterns, trends, and insights within the vast amounts of case surveillance data provided by the CDC.
           This project is a collaborative effort, aimed at enhancing our understanding of the pandemic's impact across different geographies and demographics.""")
 
@@ -54,4 +54,4 @@ st.markdown("""Here, you'll find a comprehensive analysis structured into variou
 - **Conclusion:** Reflect on the implications of our findings and the potential paths forward.""")
 
 st.info("""**Acknowledgement**  
-This project was completed by students in the Intermediate Applied Data Analysis class within the Biomedical Informatics Program at the University of Chicago. Our work represents a collaborative educational endeavor, where we applied our learning to real-world data to contribute to the broader understanding of the COVID-19 pandemic. We extend our gratitude to our instructors, peers, and the University of Chicago for fostering an environment where such impactful work can be conducted.""")
+This project was completed by students in the Intermediate Applied Data Analysis class within the Biomedical Informatics Program at the University of Chicago. Our work represents a collaborative educational endeavor, where we applied our learning to real-world data to contribute to the broader understanding of the COVID-19 pandemic.""")
